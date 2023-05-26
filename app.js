4e3e532ad8703c505e79a01cebcab7dd4c799ee2
Raj Anand <58350132+Just-A-Pixel@users.noreply.github.com>
2023-05-27 01:00:00 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,3 +1,3 @@
 window.location.replace(
-    "https://drive.google.com/file/d/1awsjjgl-j-xvFYv1m3ArQFcGw3lDhxya/view?usp=sharing"
+    "https://drive.google.com/file/d/1uKTlfQj6n5-YNATCipSsvZqpAVVJImN9/view?usp=sharing"
   );
